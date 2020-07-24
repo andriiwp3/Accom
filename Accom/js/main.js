8081eb3320101041e671b585ad1dbeb7109e29ca
andriiwp3 <andriiwp3@gmail.com>
2020-07-24 17:16:44 +0300

    Made a section «What do we take»

--- Accom/js/main.js
+++ Accom/js/main.js
@@ -1020,13 +1020,38 @@ function tip() {
 }
 let menuLinks = document.querySelectorAll('.menu__link');
 
-menuLinks.forEach(function(link) {
+menuLinks.forEach(function (link) {
 	link.addEventListener('click', function (e) {
 		let activeMenuLink = document.querySelector('.menu__link.active');
 
 		e.preventDefault();
 		link.classList.add('active');
 		activeMenuLink.classList.remove('active');
+	});
+});
+
+let whatDoWeTakeSpollers = document.querySelectorAll('.spollers-what-do-we-take__title.spoller');
+let spollersWhatDoWeTakeImages = document.querySelectorAll('.images-what-do-we-take__img');
+
+whatDoWeTakeSpollers.forEach(function (spoller, i) {
+	spoller.setAttribute('data-wdwt-spoller-index', i);
+
+	spoller.addEventListener('click', function (e) {
+		let spollerWhatDoWeTakeItem = spoller.closest('.spollers-what-do-we-take__item');
+
+		if (!spollerWhatDoWeTakeItem.classList.contains('active')) {
+			let activeSpollerWhatDoWeTakeItem = document.querySelector('.spollers-what-do-we-take__item.active');
+			let activeSpollerWhatDoWeTakeImg = document.querySelector('.images-what-do-we-take__img.active');
+
+			if (activeSpollerWhatDoWeTakeItem) {
+				activeSpollerWhatDoWeTakeItem.classList.remove('active');
+			}
+			spollerWhatDoWeTakeItem.classList.add('active')
+			activeSpollerWhatDoWeTakeImg.classList.remove('active');
+			spollersWhatDoWeTakeImages[spoller.dataset.wdwtSpollerIndex].classList.add('active');
+		} else {
+			spollerWhatDoWeTakeItem.classList.remove('active');
+		}
 	});
 });
 // HTML data-da="where(uniq class name),position(digi),when(breakpoint)"
