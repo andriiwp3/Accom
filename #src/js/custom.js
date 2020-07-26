75d53dd71b871eada79d5c4bbdc35a6c85b81230
andriiwp3 <andriiwp3@gmail.com>
2020-07-26 21:17:57 +0300

    Made a section with services

--- #src/js/custom.js
+++ #src/js/custom.js
@@ -1,3 +1,6 @@
+/*
+ * Menu
+*/
 let menuLinks = document.querySelectorAll('.menu__link');
 
 menuLinks.forEach(function (link) {
@@ -10,6 +13,11 @@ menuLinks.forEach(function (link) {
 	});
 });
 
+
+
+/*
+* Spollers in section «What do we take»
+*/
 let whatDoWeTakeSpollers = document.querySelectorAll('.spollers-what-do-we-take__title.spoller');
 let spollersWhatDoWeTakeImages = document.querySelectorAll('.images-what-do-we-take__img');
 
@@ -34,3 +42,123 @@ whatDoWeTakeSpollers.forEach(function (spoller, i) {
 		}
 	});
 });
+
+
+
+/*
+	* Functionality for the "See More" button
+*/
+let limitedBlocks = document.querySelectorAll('._limited');
+
+limitedBlocks.forEach(function (block) {
+	let limLimit = block.getAttribute('data-limited-height');
+	let limArr = limLimit.split(',');
+	let limDesk = limArr[0] ? limArr[0].trim() : '50';
+	let limMob = limArr[1] ? limArr[1].trim() : '30';
+	let limBreakpoint = limArr[2] ? limArr[2].trim() : '550.98';
+	let limMargin = limArr[3] ? limArr[3].trim() : '30';
+	let limColOnDesk = limArr[4] ? limArr[4].trim() : '2';
+
+	limCheckMedia(block, limDesk, limMob, limBreakpoint, limMargin, limColOnDesk);
+});
+
+// We get all the buttons on the page.
+let btnSeeMore = document.querySelectorAll('.btn-see-more');
+
+btnSeeMore.forEach(function (btn) {
+
+	// We get a section that needs to be limited in height.
+	let limBlock = btn.parentElement.querySelector('._limited');
+	let limLimit = limBlock.getAttribute('data-limited-height');
+
+	if (limLimit != '') {
+		let limArr = limLimit.split(',');
+		let limDesk = limArr[0] ? limArr[0].trim() : '50';
+		let limMob = limArr[1] ? limArr[1].trim() : '30';
+		let limBreakpoint = limArr[2] ? limArr[2].trim() : '550.98';
+		let limMargin = limArr[3] ? limArr[3].trim() : '30';
+		let limColOnDesk = limArr[4] ? limArr[4].trim() : '2';
+
+		if (limArr.length > 0) {
+			btn.addEventListener('click', function (e) {
+				e.preventDefault();
+
+				// Add classes with predefined styles to the button and change the text.
+				btn.classList.add('lock');
+				btn.classList.toggle('active');
+				changeTextOnSeeMoreBtn(btn);
+
+				// We add classes to the necessary block and call the function that will limit it by height. Also, after the animation we remove the class that prohibits clicking on the button.
+				limBlock.classList.toggle('active');
+
+				limCheckMedia(limBlock, limDesk, limMob, limBreakpoint, limMargin, limColOnDesk);
+				setTimeout(() => {
+					btn.classList.remove('lock');
+				}, 1500);
+			})
+		}
+	}
+});
+
+window.addEventListener('resize', function (e) {
+	let limLimit = limitedBlocks[0].getAttribute('data-limited-height');
+	let limArr = limLimit.split(',');
+	let limDesk = limArr[0] ? limArr[0].trim() : '50';
+	let limMob = limArr[1] ? limArr[1].trim() : '30';
+	let limBreakpoint = limArr[2] ? limArr[2].trim() : '550.98';
+	let limMargin = limArr[3] ? limArr[3].trim() : '30';
+	let limColOnDesk = limArr[4] ? limArr[4].trim() : '2';
+
+	limCheckMedia(limitedBlocks[0], limDesk, limMob, limBreakpoint, limMargin, limColOnDesk);
+});
+
+function limCheckMedia(block, limitedHeightDesk = 50, limitedHeightMob = 30, limitedBreakpoint = 550.98, limitedMargin = 30, limitedColOnDesk = 2) {
+	let blockChildren = [].slice.call(block.children);
+
+	// We check to see if the block needs to be restricted
+	/* if (blockFullHeight < limitedHeight) return; */
+
+	if (window.matchMedia('(max-width: ' + limitedBreakpoint + 'px').matches) {
+		let childrenFullHeight = 0;
+
+		blockChildren.forEach(function (child) {
+			childrenFullHeight += child.getBoundingClientRect().height + +limitedMargin;
+		});
+
+		blockLimit(block, childrenFullHeight + 'px', childrenFullHeight / 100 * limitedHeightMob + 'px');
+	}
+
+	if (!window.matchMedia('(max-width: ' + limitedBreakpoint + 'px').matches) {
+		let childrenFullHeight = 0;
+
+		blockChildren.forEach(function (child) {
+			childrenFullHeight += child.getBoundingClientRect().height + +limitedMargin;
+		});
+
+		childrenFullHeight /= +limitedColOnDesk;
+
+		blockLimit(block, childrenFullHeight + 'px', childrenFullHeight / 100 * limitedHeightDesk + 'px');
+	}
+}
+
+function blockLimit(block, blockFullHeight, limitedHeight) {
+	// Limiting the block.
+	if (!block.classList.contains('active')) {
+		block.style.height = limitedHeight;
+		return;
+	}
+
+	// Returning the block to full height.
+	if (block.classList.contains('active')) {
+		block.style.height = blockFullHeight;
+		return;
+	}
+}
+
+function changeTextOnSeeMoreBtn(btn) {
+	if (btn.classList.contains('active')) {
+		btn.textContent = 'Смотреть меньше';
+		return;
+	}
+	btn.textContent = 'Смотреть все услуги';
+}
